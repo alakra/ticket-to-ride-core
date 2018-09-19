5bdef6877495c047c8bc594dc13ab124744a9d28
Angelo Lakra <angelo.lakra@gmail.com>
2018-09-18 21:51:14 -0600

    Made various mix file changes
    * Changed ex_doc to 0.19 (for 1.7.3)
    * Removed excoveralls (warnings from 1.7.3 changes were annoying)
    * Removed several unused dependencies from mix lockfile

--- mix.exs
+++ mix.exs
@@ -4,16 +4,11 @@ defmodule TtrCore.Mixfile do
   def project do
     [app: :ttr_core,
      version: "1.0.0",
-     elixir: "~> 1.6",
+     elixir: "~> 1.7",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      aliases: aliases(),
-     deps: deps(),
-     test_coverage: [tool: ExCoveralls],
-     preferred_cli_env: ["coveralls": :test,
-                         "coveralls.detail": :test,
-                         "coveralls.post": :test,
-                         "coveralls.html": :test]
+     deps: deps()
     ]
   end
 
@@ -33,14 +28,11 @@ defmodule TtrCore.Mixfile do
       # All
       {:uuid, "~> 1.1"},
 
-      # Test
-      {:excoveralls, "~> 0.9", only: :test},
-
       # Development
       {:benchee, "~> 0.13", only: :dev},
       {:credo, "~> 0.10.0", only: :dev},
       {:dialyzex, "~> 1.1", only: :dev},
-      {:ex_doc, "~> 0.18", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.19", only: :dev, runtime: false},
     ]
   end
 end
